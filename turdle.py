7c3c7cdffcbaa19950f878dd5a5acdd8f9084530
bhoff <bruce.hoff@sagebase.org>
2022-05-26 07:32:21 -0700

    additional functions

--- turdle.py
+++ turdle.py
@@ -1,10 +1,13 @@
 import os, re, json
 from os.path import exists
 from math import log2
+import time
+from collections import OrderedDict
 
 WORD_FILE = "corpus.txt"
 FIRST_GUESS_FILE = "first_guess.json"
-NUM_GOOD_GUESSES = 50
+#NUM_GOOD_GUESSES = 50
+NUM_GOOD_GUESSES = 2315
 HARD_MODE = False
 MIN_EASY_MODE_POSSIBLE_ANSWER_SIZE = 0 # normally 3 but setting to 0 disables the feature
 
@@ -60,11 +63,9 @@ def get_hint_as_string(guess, target):
 				imperfect_match_ctr = imperfect_match_ctr + 1
 				
 	return "".join(result)
-		
+
 # '--yg-' -> 1*9+2*3 = 15
 def hint_value_from_string(s):
-	if len(s)!=5:
-		raise Exception("Hint must have length five")
 	multiplier = 3
 	result = 0
 	for i in range(0,len(s)):
@@ -74,7 +75,7 @@ def hint_value_from_string(s):
 		elif s[i]=='y':
 			result = result + 1 # yellow
 		elif s[i]!='-':
-			raise Exception("Hint must cntsin only '-', 'y', 'g'")
+			raise Exception("Hint must contain only '-', 'y', 'g'")
 		# else grey
 	return result
 	
@@ -89,6 +90,24 @@ def partition(guess, possible_answers):
 		hint_set.append(target)
 		partition[hint]=hint_set
 	return partition
+	
+# given a list of guesses and a target, return the hints as one long string
+def get_hints_as_string(guesses, target):
+	result = ""
+	for guess in guesses:
+		hint = get_hint_as_string(guess, target)
+		result = result+hint
+	return result
+		
+def partition_from_multiple_guesses(guesses, possible_answers):
+	partition = {}
+	for target in possible_answers:
+		hint = hint_value_from_string(get_hints_as_string(guesses, target))
+		hint_set = partition.get(hint, [])
+		hint_set.append(target)
+		partition[hint]=hint_set
+	return partition
+
 
 #
 # Original rank is the average size of the resultant list after
@@ -109,11 +128,13 @@ def rank_partition(partition):
 	for value in partition.values():
 		n = len(value)
 		# original:
-		# result = result + n*n
+		result = result + n*n
 		# information theoretic:
-		result = result + n*log2(n/total)
+		# result = result + n*log2(n/total)
 	return result/total
 
+# returns a list of guesses, ordered by their rank
+# result is a list of dict's with keys, 'rank', 'guess', 'partition'
 def optimize_guess(possible_guesses, possible_answers):
 	# If the number of possible answers is small, don't use 'easy mode', in which a guess
 	# can be outside the list of possible answers.  Just use 'hard mode', i.e., only guess
@@ -167,6 +188,70 @@ def rank_all_words(words, initial_guesses):
 		stats[guess_counter] = stats.get(guess_counter,0)+1
 		#print(f"Target word {target} is guessed in {guess_counter} guesses: {guesses}.")
 	print(stats)
+	
+def best_second_guess(words, initial_guesses):
+	best_initial_guess = initial_guesses[0]
+	print(f"Best initial guess is {best_initial_guess['guess']}")
+
+	result = [] # list of dict's with keys, 'guess' and 'rank'
+
+	for candidate_second_guess in words:
+		# best_initial_guess["partition"] is a dict 
+		# mapping 'hints' to the set of words that will generate that hint
+		weighted_second_guess_rank = 0
+		count = 0
+		for hint in best_initial_guess["partition"]:
+			possible_answers = best_initial_guess["partition"][hint]
+			
+			# how does candidate_second_guess partition possible_answers?
+			partition_for_guess = partition(candidate_second_guess, possible_answers)
+			guess_rank = rank_partition(partition_for_guess)			
+			
+			
+			weighted_second_guess_rank = weighted_second_guess_rank + guess_rank*len(possible_answers)
+			count = count + len(possible_answers)
+
+		rank = weighted_second_guess_rank / count
+		result.append({'guess':candidate_second_guess, 'rank':rank})
+		print(f'Second guess {candidate_second_guess} has rank {rank}.')
+		
+		
+	result.sort(key=lambda item:item.get('rank'))
+			
+	print("Second guesses:")
+	for i in range(0,50):
+		print(f"Second guess: {result[i]['guess']}, rank: {result[i]['rank']}")
+
+	#print(result)
+	
+def best_multi_guess(word_set):
+	best_rank = None
+	
+	words = list(word_set)
+	for i in range(0, len(words)):
+		print(f'{i} of {len(words)} [{words[i]}, ----]')
+		for j in range(i+1,len(words)):
+			guesses = [words[i], words[j]]
+			partition = partition_from_multiple_guesses(guesses, words)
+			rank = rank_partition(partition)
+			if best_rank is None or rank<best_rank:
+				best_rank=rank
+				best_guesses = guesses
+				print(f'Best guesses: {best_guesses}, rank={best_rank}')
+				
+				
+				
+def rank_select_word_pairs(word_set):
+	words = list(word_set)
+	rank_select_word_pair(['salon', 'trice'], words)
+	rank_select_word_pair(['raise', 'clout'], words)
+	rank_select_word_pair(['learn', 'pouty'], words)
+	
+def rank_select_word_pair(pair, words):
+	partition = partition_from_multiple_guesses(pair, words)
+	rank = rank_partition(partition)
+	print(f'rank for {pair} is {rank}.')
+		
 			
 def read_initial_guess_file():
 	with open(FIRST_GUESS_FILE) as infile:
@@ -187,7 +272,9 @@ def main():
 		print("Using EASY mode.")
 	words = create_list()
 	if not exists(FIRST_GUESS_FILE):
+		start = time.perf_counter()
 		best_guesses = optimize_guess(words, words)
+		print(f"{time.perf_counter()-start} seconds to rank initial guesses")
 		with open(FIRST_GUESS_FILE, "w") as outfile:
 			json.dump(best_guesses, outfile)
 	initial_guesses = read_initial_guess_file()
@@ -196,13 +283,17 @@ def main():
 		rank_all_words(words, initial_guesses)
 		return 0
 		
+	if False: # rank all words
+		best_second_guess(words, initial_guesses)
+		return 0
 		
-	# hint = "--yy-"
-	# print(f"value for {hint} {hint_value_from_string(hint)}")
-	#guess = "crowd"
-	#target = "aroma"
-	#print(f"Hint for guess {guess} and target {target}: {get_hint_as_string(guess, target)}")
-	#return 0
+	if False: # rank all words
+		best_multi_guess(words)
+		return 0
+		
+	if False: # rank all words
+		rank_select_word_pairs(words)
+		return 0
 		
 	# interactive application
 	possible_answers = words
@@ -236,6 +327,8 @@ def main():
 			raise Exception("You must choose one of the suggested guesses.")
 				
 		wordle_response = input("Enter wordle's response (5 chars of 'g', 'y', or '-'): ")
+		if len(wordle_response)!=5:
+			raise Exception("Hint (Wordle response) must have length five.")
 		wordle_hint = hint_value_from_string(wordle_response)
 		try:
 			possible_answers = best_partition[wordle_hint]
